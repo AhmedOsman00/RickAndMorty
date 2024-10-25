5b84a8dc1abc456378df091d230a05eb29ae8a39
Ahmed Osman <eng.ahmedosman00@gmail.com>
2024-10-25 09:13:58 +0200

    init project

---
+++ rickyandmorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rickyandmorty
+//
+//  Created by ahmed osman on 25.10.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
